09b522a503f93de227b0d266387a42168210dab6
Hudson Gouge <hudson.gouge@icloud.com>
2024-03-19 19:42:26 -0400

    improved shebang for example and package instructions.

--- package.sh
+++ package.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+
+# Steps to run:
+# 1. Go one directory up
+# 2. Duplicate the Aardvark directory.
+# 3. Rename the duplicate "adk"
+# 4. Run this script:
+
 zip -r adk.zip adk -x "adk/.git/*" "adk/__pycache__/*" "adk/.gitignore" "adk/Aardvark Interpreter/__pycache__/*" "adk/Tensovark/__pycache__/*" "adk/package.sh"
